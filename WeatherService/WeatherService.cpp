156ecfb41c471b84f25bb772cbc9732d5073220f
MaksMikr <mmaksim12120@gmail.com>
2026-06-08 09:45:39 +0300

    Добавил комментарии(XML-документации)

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -5,32 +5,46 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-#include <nlohmann/json.hpp>
-=======
 #include "nlohmann/json.hpp"
 #include "pugixml.hpp"
 using namespace pugi;
->>>>>>> feature-xml
 using json = nlohmann::json;
 
+/// <summary>
+/// Класс для хранения информации о погоде
+/// </summary>
+/// <remarks>
+/// Содержит основные параметры погоды: город, координаты, температуру,описание погоды, скорость ветра и облачность.
+/// </remarks>
 class Weather {
 private:
-    string city;
-    double lon;
-    double lat;
-    double temperature;
-    string weather;
-    double windSpeed;
-    int clouds;
+    string city;                  /// <summery>Название города</summery>
+    double lon;                   /// <summery>Долгота</summery>
+    double lat;                   /// <summery>Широта</summery>
+    double temperature;           /// <summery>Температура в градумах Цельсия</summery>
+    string weather;               /// <summery>Краткое описание погоды</summery>
+    double windSpeed;             /// <summery>Скорость ветра в м/с</summery>
+    int clouds;                   /// <summery>Облачность в процентах</summery>
 
 public:
+ /// <summary>
+ /// Конструктор класса Weather
+ /// </summary>
+ /// <param name="city">Название города</param>
+ /// <param name="lon">Долгота</param>
+ /// <param name="lat">Широта</param>
+ /// <param name="temperature">Температура (°C)</param>
+ /// <param name="weather">Описание погоды</param>
+ /// <param name="windSpeed">Скорость ветра (м/с)</param>
+ /// <param name="clouds">Облачность (%)</param>
     Weather(string city, double lon, double lat,
         double temperature, string weather,
         double windSpeed, int clouds)
         : city(city), lon(lon), lat(lat), temperature(temperature),
         weather(weather), windSpeed(windSpeed), clouds(clouds) {}
-
+    /// <summmary>
+    /// Выводим информацию о погоде 
+    /// </summmary>
     void print() const {
         cout << "Информация о погоде: " << endl;
         cout << "Город: " << city << endl;
@@ -41,23 +55,23 @@ public:
         cout << "Облачность: " << clouds << "%" << endl;
     }
 };
-
+/// <summary>Абстрактный класс для сервисов погоды</summary>
 class Service {
 public:
+    /// <summary>Получить погоду из файла</summary>
+    /// <param name="s">Путь к файлу</param>
+    /// <returns>Объект Weather</returns>
+    /// <exception cref="runtime_error">Ошибка при чтении файла</exception>
     virtual Weather getWeather(string s) = 0;
     virtual ~Service() {}
 };
-
+/// <summary>Сервис для JSON-файлов</summary>
 class JsonService : public Service {
 public:
     virtual Weather getWeather(string s) override {
         ifstream fin(s);
         if (!fin.is_open()) {
-<<<<<<< HEAD
-            throw runtime_error("Не удалось открыть файл");
-=======
             throw runtime_error("Не удалось открыть JSON файл");
->>>>>>> feature-xml
         }
 
         json j;
@@ -74,14 +88,15 @@ public:
         return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
     }
 };
-
-<<<<<<< HEAD
-int main() {
-    setlocale(LC_ALL, "rus");
-
-=======
+/// <summary>Сервис для XML-файлов</summary>
 class XmlService : public Service {
 public:
+    /// <summary>
+    /// Получает погоду из XML-файла
+    /// </summary>
+    /// <param name="s">Путь к XML-файлу</param>
+    /// <returns>Объект Weather с данными из файла</returns>
+    /// <exception cref="runtime_error">Файл не найден или имеет неверный формат</exception>
     virtual Weather getWeather(string s) override {
         pugi::xml_document doc;
         pugi::xml_parse_result result = doc.load_file(s.c_str());
@@ -102,21 +117,20 @@ public:
         return Weather(city, lon, lat, temperature, weather, windSpeed, clouds);
     }
 };
-
+/// <summary>
+/// Главная функция программы
+/// </summary>
+/// <returns>0 при успешном выполнении</returns>
 int main() {
     setlocale(LC_ALL, "rus");
 
     cout << "=== Проверка JSON ===" << endl;
->>>>>>> feature-xml
     try {
         JsonService js;
         Weather w = js.getWeather("weather.json");
         w.print();
     }
     catch (const exception& e) {
-<<<<<<< HEAD
-        cout << "Ошибка: " << e.what() << endl;
-=======
         cout << "Ошибка JSON: " << e.what() << endl;
     }
 
@@ -128,7 +142,6 @@ int main() {
     }
     catch (const exception& e) {
         cout << "Ошибка XML: " << e.what() << endl;
->>>>>>> feature-xml
     }
 
     return 0;
